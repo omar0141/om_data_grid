8cea9d1f974f5b0826330b60fea688109ec4865a
omar0141 <omarran2000@hotmail.com>
2026-02-23 11:51:08 +0200

    new

--- example/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/omar/Porjects/data_grid/example/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/omar/Porjects/data_grid/example/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
--- example/build/macos/Build/Intermediates.noindex/Pods.build/Debug/file_selector_macos.build/Objects-normal/arm64/file_selector_macos_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/omar/Porjects/data_grid/example/build/macos/Build/Intermediates.noindex/Pods.build/Debug/file_selector_macos.build/module.modulemap \
-  /Users/omar/Porjects/data_grid/example/build/macos/Build/Intermediates.noindex/Pods.build/Debug/file_selector_macos.build/DerivedSources/file_selector_macos_vers.c
